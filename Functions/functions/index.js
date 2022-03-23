48e98fab87f5c3a27cc7c3ef9138208f3f85bd1d
magiri <ndung'u.victor19@students.dkut.ac.ke>
2022-03-23 17:09:14 +0300

    Fix Some Issues

--- Functions/functions/index.js
+++ Functions/functions/index.js
@@ -1,20 +1,5 @@
 const functions = require("firebase-functions");
 const admin=require("firebase-admin");
 admin.initializeApp(functions.config().firebase);
-exports.notifyVendor=functions.database.ref("Notifications/{userID}/").onWrite((data,context)=>{
-    console.log("Push Notification triggered");
-    const valueObject=change.after.val();
 
-    const payLoad={
-        notification:{
-            title:valueObject.title,
-            body:valueObject.message,
-            sound:"default"
-        }
-    };
-    const options={
-        priority:"high",
-        timeToLive:60*60*24
-    };
-    return admin.messaging.sendToTopic("Notify Vendor",payload,options);
 });
